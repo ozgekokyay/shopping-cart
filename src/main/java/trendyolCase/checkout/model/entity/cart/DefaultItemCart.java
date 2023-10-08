cdc44681b4539d93f90b0ea4970344b76f31aaf6
ozgekokyay <ozgekokyay2@gmail.com>
2023-10-08 20:23:23 +0300

    shopping-cart-rest-api-endpoints-implemented

--- src/main/java/trendyolCase/checkout/model/entity/cart/DefaultItemCart.java
+++ src/main/java/trendyolCase/checkout/model/entity/cart/DefaultItemCart.java
@@ -1,6 +1,5 @@
 package trendyolCase.checkout.model.entity.cart;
 
-import jakarta.persistence.Entity;
 
 public class DefaultItemCart extends Cart {
 
--- src/main/java/trendyolCase/checkout/model/entity/cart/DigitalItemCart.java
+++ src/main/java/trendyolCase/checkout/model/entity/cart/DigitalItemCart.java
@@ -1,8 +1,4 @@
 package trendyolCase.checkout.model.entity.cart;
-
-import jakarta.persistence.Entity;
-
-
 public class DigitalItemCart extends Cart {
 
 }
